f409ce9af7bc444e6fffb2fce1e96833bedf5bda
yadhuct0-lang <yadhuct0@gmail.com>
2025-11-04 13:00:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ chmod 400 KUBE-INFRA-KP.pem
 mkdir -p ~/.ssh
 echo "Host *\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n" > ~/.ssh/config
 chmod 600 ~/.ssh/config
-scp -i KUBE-INFRA-KP.pem web/* ubuntu@172.31.14.22:/usr/share/nginx/html
+scp -i KUBE-INFRA-KP.pem web/* ubuntu@172.31.14.22:/usr/share/nginx/html/
 ssh -i KUBE-INFRA-KP.pem ubuntu@172.31.14.22 'sudo systemctl restart nginx'
